b27e752ecf70d681e7801f13281a3b4f79d37829
user910120 <user910120@74-80-245-38.ic2net.net>
2018-04-26 15:36:27 -0700

    PROJECT 1 COMPLETED

--- ProjectOneKai/ViewController.swift
+++ ProjectOneKai/ViewController.swift
@@ -8,16 +8,48 @@
 
 import UIKit
 
+//Variable Coding Challenge #1
+var firstString = "The Beginning"
+var secondString = "and The End"
+var thirdString = firstString + secondString
+print(thirdString)
+
+//Variable Coding Challenge #2
+var celsius: Float
+let farenheit: Float = 50
+let celsius = 5/9 * (farenheit-32)
+print(celsius)
+
+//Boolean Variables Coding Challenge #1
+var numberOne: Int
+var numberTwo: Int
+var numberThree = numberOne == numberTwo
+
+//If Statement Coding Challenge #1
+var stringOne = "Swift"
+var stringTwo = "Code"
+let length1 = stringOne.count
+let length2 = stringTwo.count
+if length1>length2 {
+    print(stringOne)
+}
+else {
+    print(stringTwo)
+}
+
+
 class ViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+     
     }
 
 
